684119e59ed00eae83e1e75c730e85c648451327
Soorej <soorejsbabu@gmail.com>
2024-12-04 16:15:24 +0530

    Initial Commit

---
+++ Soorej-Babu-AnswersAi-iOS/Soorej_Babu_AnswersAi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soorej_Babu_AnswersAi_iOSApp.swift
+//  Soorej-Babu-AnswersAi-iOS
+//
+//  Created by Soorej Babu on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Soorej_Babu_AnswersAi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
